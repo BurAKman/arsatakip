f09661f51a8cc974222546a16e9b7dcff66c45e2
BurAKman <b.akmanweb@gmail.com>
2026-09-18 20:29:03 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Strateji: HTML her zaman once agdan (guncelleme aninda gelsin),
 // gorseller ve kutuphaneler once onbellekten (hizli acilsin).
 
-const SURUM = "cc-v5";
+const SURUM = "cc-v6";
 const TEMEL = [
   "./",
   "./index.html",
